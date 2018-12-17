d0dd5f6da55d15e035526378862c6fdbe7205c0f
Shashank <shashank@gmail.com>
2018-12-17 00:36:55 -0600

    Added API to support category like search with zip-code

--- src/main/java/com/datingapp/db/EventDAOImpl.java
+++ src/main/java/com/datingapp/db/EventDAOImpl.java
@@ -1,15 +1,24 @@
 package com.datingapp.db;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
 
+import com.datingapp.eb.model.Event;
+import com.datingapp.services.LocationService;
 import com.datingapp.util.ResultSetAdapter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
 
-public class EventDAOImpl extends RdsJdbcHandler{
+public class EventDAOImpl extends RdsJdbcHandler {
+	
+	private LocationService  locationService = new LocationService();
 
 	public EventDAOImpl() {
 		super();
@@ -30,7 +39,7 @@ public class EventDAOImpl extends RdsJdbcHandler{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	@Override
@@ -38,9 +47,7 @@ public class EventDAOImpl extends RdsJdbcHandler{
 		if (map.isEmpty())
 			return null;
 		StringBuilder sb = new StringBuilder();
-		String listOfColumns = "id, source, display_name, url, address1, address2, city, state, zip_code, country, latitude, longitude, start_time, end_time, price, deal_id, image_url, category_name";
-		sb.append("Select " + listOfColumns
-				+ " from event where ");
+		addListOfColumns(sb);
 		for (String column : map.keySet()) {
 			sb.append(column + " = ");
 			sb.append("'" + map.get(column) + "'");
@@ -48,12 +55,12 @@ public class EventDAOImpl extends RdsJdbcHandler{
 		}
 		sb.delete(sb.length() - 5, sb.length());
 		sb.append(";");
-		
+
 		Statement statement;
 		try {
 			statement = this.dbconnection.createStatement();
 			ResultSet results = statement.executeQuery(sb.toString());
-			if(results!=null) {
+			if (results != null) {
 				JSONArray array = ResultSetAdapter.convertToJSON(results);
 				return array.toString();
 			}
@@ -65,4 +72,62 @@ public class EventDAOImpl extends RdsJdbcHandler{
 		return null;
 	}
 
+	/**
+	 * Function to retreive categories.
+	 * 
+	 * @return
+	 */
+	public String getEventCategories(String categoryName,String zipcode) {
+		List<String> nearByZipcodes = new ArrayList<String>();
+		
+		StringBuilder categorySearchBuilder = new StringBuilder();
+		addListOfColumns(categorySearchBuilder);
+		categorySearchBuilder.append(" upper(trim(category_name)) like");
+		categorySearchBuilder.append(" '%");
+		categorySearchBuilder.append(categoryName != null ? categoryName.trim().toUpperCase() : " ");
+		categorySearchBuilder.append(" %' ");
+		if(zipcode!=null) {
+			nearByZipcodes = locationService.nearbyZipcodesTo(zipcode);
+			categorySearchBuilder.append("   AND zip_code in (  ");
+			for(int i=0 ; i < nearByZipcodes.size() ; i++) {
+				if(i == nearByZipcodes.size() - 1) {
+					categorySearchBuilder.append( "'"+ nearByZipcodes.get(i) 	+ "'" + "  ");	
+				} else {
+					categorySearchBuilder.append( "'"+ nearByZipcodes.get(i) 	+ "'" + ",");
+				}
+				
+			}
+			categorySearchBuilder.append("  ) ");
+			
+		}
+
+		categorySearchBuilder.append(";");
+		
+		Statement statement;
+		try {
+			getConnection();
+			statement = this.dbconnection.createStatement();
+			ResultSet results = statement.executeQuery(categorySearchBuilder.toString());
+			if (results != null) {
+				JSONArray array = ResultSetAdapter.convertToJSON(results);
+				return array.toString();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param sb
+	 */
+	private void addListOfColumns(StringBuilder sb) {
+		String listOfColumns = "id, source, display_name, url, address1, address2, city, state, zip_code, country, latitude, longitude, start_time, end_time, price, deal_id, image_url, category_name";
+		sb.append("Select " + listOfColumns + " from event where ");
+	}
+
+	
+	
 }
